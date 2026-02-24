3f6cba01c77907f44f386fe441908b84fb675306
River <fp32@duck.com>
2026-02-24 12:52:08 +0000

    Static builds to GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import vue from '@astrojs/vue';
-
-import cloudflare from '@astrojs/cloudflare';
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue()],
-  adapter: cloudflare()
+  integrations: [],
 });
