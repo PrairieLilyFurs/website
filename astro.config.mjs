0900865af21403f6551aeaab327924c8653d949a
River <fp32@duck.com>
2026-02-24 13:14:11 +0000

    Add build timestamp

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,18 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import { execSync } from 'node:child_process';
+
+// @ts-check
+
+const gitHash = execSync('git rev-parse --short HEAD').toString().trim();
+const buildTime = new Date().toISOString();
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [],
+  vite: {
+    define: {
+      'import.meta.env.GIT_HASH': JSON.stringify(gitHash),
+      'import.meta.env.BUILD_TIME': JSON.stringify(buildTime)
+    }
+  }
 });
